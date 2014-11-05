0adec237d1a924adcdf2e63a3af06474781ffda2
matthew-gadd <matthew.gadd@thomsonreuters.com>
2014-11-05 12:04:50 +0000

    first commit, some sample data

---
+++ data/subjects.js
@@ -0,0 +1,8 @@
+define([], function () {
+	return [
+		{"label": "A-Team Episodes", "module": "a-team-episodes"},
+		{"label": "Hindu Gods", "module": "hindu-gods"},
+		{"label": "Left Handed Actors", "module": "left-handed-actors"},
+		{"label": "Doctor Who Planets", "module": "dr-who-planets"}
+	];
+});
