cf6d6d39855e7e5957b209f78a4b85f23770349d
Tom Dye <tom.dye@thomsonreuters.com>
2014-11-06 16:13:51 +0000

    working hack

--- src/main.js
+++ src/main.js
@@ -2,25 +2,82 @@ import DataLoader from "src/DataLoader";
 import WordSolver from "src/WordSolver";
 import subjects from "data/subjects";
 
-var currentSubjectIndex;
+var currentSubjectIndex,
+	wordLengths,
+	loadedSubjects;
 
 window.addEventListener('push', () => {
-	var hash = window.location.hash
-	var subjectMatch = hash.match(/subject\=(.*)/);
-	if (subjectMatch) {
-		currentSubjectIndex = subjectMatch[1];
+	var hash = window.location.hash;
+
+	if (document.getElementById('input')) {
+		var subjectMatch = hash.match(/subject\=(.*)/);
+		if (subjectMatch) {
+			currentSubjectIndex = subjectMatch[1];
+		}
+
+		document.getElementById("wordsButton").addEventListener("touchstart", function (evt) {
+			var wordsForm = document.forms[0];
+			var inputValues = Array.prototype.slice.apply(wordsForm.children).map(function (input) {
+				return input.value - 0;
+			});
+			wordLengths = inputValues.filter(function (val) {
+				return !!val;
+			});
+		});
+	} else if (document.getElementById('words')) {
+		var thisSubject = loadedSubjects[currentSubjectIndex];
+		var wordSolver = new WordSolver(thisSubject.words, wordLengths[0]);
+		var letterHolder = document.getElementById("letter");
+		var wordsList = document.getElementById("possible-words");
+
+		wordSolver.start();
+
+		var currentLetter, possibleWords;
+
+		document.getElementById("yesButton").addEventListener("touchend", function (evt) {
+			possibleWords = wordSolver.yes(currentLetter);
+			currentLetter = wordSolver.next();
+			drawStuff();
+		});
+
+		document.getElementById("noButton").addEventListener("touchend", function (evt) {
+			possibleWords = wordSolver.no(currentLetter);
+			currentLetter = wordSolver.next();
+			drawStuff();
+		});
+
+		function drawStuff() {
+			var wordsListStr = "";
+			letterHolder.innerHTML = currentLetter;
+
+
+			if (possibleWords) {
+				possibleWords.forEach(function (word) {
+					wordsListStr += "<li class='table-view-cell'>" + word + "</li>";
+				});
+
+				wordsList.innerHTML = wordsListStr;
+			}
+
+		}
+
+		 currentLetter = wordSolver.next();
+		 drawStuff();
+
+
+
+
+
+
 	}
 });
 
 var main = document.getElementById("main");
 var dataLoader = new DataLoader(subjects);
 dataLoader.start().then((subjects) => {
-	var aTeamSubject = subjects[0];
-	var wordSolver = new WordSolver(aTeamSubject.words, 4);
-	wordSolver.start();
-window.wordSolver = wordSolver;
+	loadedSubjects = subjects;
 
-
+	var subjectsList = document.getElementById("subjectsList");
 	subjects.forEach((subject, i) => {
 		var li = document.createElement("li");
 		li.classList.add("table-view-cell");
@@ -29,8 +86,7 @@ window.wordSolver = wordSolver;
 		anchor.innerHTML = subject.label;
 		anchor.href = "./input.html#subject=" + i;
 		li.appendChild(anchor);
-		var subjectsList = document.getElementById("subjectsList");
 		subjectsList.appendChild(li);
 	});
-})
+});
 
